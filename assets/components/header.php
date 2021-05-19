5fe01f9a192fa8f561cead411df231c72423a853
workOfYasir <yasirb673@gmail.com>
2021-05-19 17:10:28 +0500

    final changes
    Signed-off-by: workOfYasir <yasirb673@gmail.com>

---
+++ assets/components/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<!-- <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="" href="./assets/css/style.css">
+    <title>Document</title>
+   
+</head>
+<body> 
+<div class="container-fluid" style="background-color:#fff">
+    <div class="row">
+<div class="col-md-12  col-sm-12 col-xs-12 nav">
+<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 ">
+<div class='outside-wrapper'><a data-reload="1" href="javascript:void(0)"><span class='hidden-xs image'>
+<img alt="Logo" width="343" height="35" style="height: auto;" srcset="./assets/img/brand-logo-343x35.png 1x, ./assets/img/brand-logo-686x70.png 2x" src="./assets/img/brand-logo-343x35.png" />
+</span>
+<span class='image visible-xs-inline'>
+<img alt="Logo" width="160" height="70" style="height: auto;" srcset="./assets/img/brand-logo-160x70.png 1x, ./assets/img/brand-logo-320x140.png 2x" src="./assets/img/brand-logo-160x70.png" /></div>
+</span>
+</a></div>
+<div class="col-lg-2 col-md-2 col-sm-6 col-xs-6 col-md-offset-4 col-lg-offset-4 text-div">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center text">
+Bereits <strong class="strong-nav">365.034 <i class="fas fa-arrow-circle-up" style=" transform: rotate(30deg);"></i></strong> <br/> Treppenlift-Anfragen gestellt</div>
+</div>
+</div></div>
+</div>
